b11d5e250f2e46bed111b3ba7167cc47be240fbc
Naomi Guyer <naomi@silverstripe.com>
2013-11-29 17:18:16 +1300

    Remove suffix from js and includes

--- javascript/staticsearch.js
+++ javascript/staticsearch.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 	$('#tipue_search_input').tipuesearch({
 		'mode': 'static',
-		'contentLocation': '/search_index.html'
+		'contentLocation': '/search_index'
 	});
 
 	$("form.no-pageload input").keypress(function(e){
